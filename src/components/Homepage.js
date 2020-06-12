c0b0fcf4dbebf2ae0ec4eb58c81af7795317708d
vbazanm <victor_bazan_milla@optum.com>
2020-06-12 15:01:50 +0100

    Add Homepage component

---
+++ src/components/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div className="jumbotron">
+      <h1>PluralSight Adminitration</h1>
+      <p>React, Flux, and React Router for ultra-responsive web app</p>
+    </div>
+  );
+};
+
+export default Homepage;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { render } from "react-dom";
+import Homepage from "./components/Homepage";
+
+render(<Homepage />, document.getElementById("root"));
