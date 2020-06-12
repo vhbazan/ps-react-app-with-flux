39ad0fddfe699a72be0421afa653ff23fdddf82f
vbazanm <victor_bazan_milla@optum.com>
2020-06-12 15:46:03 +0100

    Add Header navigation menu

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
+import Header from "./common/Header";
 
 function App() {
-  const route = window.location.pathname;
+  function getPage() {
+    const route = window.location.pathname;
 
-  if (route === "/about") return <AboutPage />;
-  return <HomePage />;
+    if (route === "/about") return <AboutPage />;
+    return <HomePage />;
+  }
+  return (
+    <div className="container-fluid">
+      <Header />
+
+      {getPage()}
+    </div>
+  );
 }
 
 export default App;
