7c4125e37dc6b91ebe802c0148ea4a49fbc4e5c9
vbazanm <victor_bazan_milla@optum.com>
2020-06-14 11:33:50 +0100

    Use react Link directive

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <div className="jumbotron">
       <h1>PluralSight Adminitration</h1>
       <p>React, Flux, and React Router for ultra-responsive web app</p>
-      <a href="/about">About</a>
+      <Link to="about" className="btn btn-primary">
+        About
+      </Link>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
-import { BrowserRouter as Router } from "react-routr-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 render(
   <Router>
