d04fbb728c75ddc0ae2a28d2e537e39bb81dc5dc
vbazanm <victor_bazan_milla@optum.com>
2020-06-14 12:35:52 +0100

    Add course state using react hooks

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Prompt } from "react-router-dom";
+import CourseForm from "./CourseForm";
 
 const ManageCoursePage = props => {
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    category: ""
+  });
+
   return (
     <>
       <h2>Manage Course</h2>
-      <Prompt when={true} message="Are you sure you want to leave?" />
+      <CourseForm course={course} />
       {props.match.params.slug}
     </>
   );
