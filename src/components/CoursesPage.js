d1a4ef069f1fac0e7089266b8daea99926042b8b
vbazanm <victor_bazan_milla@optum.com>
2020-06-16 15:06:57 +0100

    Add loadCourses action, register action in dispatcher and use it

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -2,13 +2,22 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import courseStore from "../stores/CourseStore";
 import CourseList from "./CourseList";
+import { loadCourses } from "../actions/courseActions";
 
 function CoursesPage() {
-  const [courses, setCourses] = useState([]);
+  const [courses, setCourses] = useState(courseStore.getCourses());
 
   useEffect(() => {
-    setCourses(courseStore.getCourses());
+    courseStore.addChangeListener(onChange);
+    if (courseStore.getCourses().length === 0) {
+      loadCourses();
+      return () => courseStore.removeChangeListener(onChange); //cleanup on unmonut
+    }
   }, []);
+
+  function onChange() {
+    setCourses(courseStore.getCourses());
+  }
 
   return (
     <div>
--- src/stores/CourseStore.js
+++ src/stores/CourseStore.js
@@ -35,7 +35,10 @@ Dispatcher.register(action => {
       _courses.push(action.course);
       store.emitChange();
       break;
-
+    case actionTypes.LOAD_COURSES:
+      _courses = action.courses;
+      store.emitChange();
+      break;
     default:
       break;
   }
