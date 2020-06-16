449cfb1da61f5ada5b5f4c44c56523e782cf9631
vbazanm <victor_bazan_milla@optum.com>
2020-06-16 10:35:19 +0100

    Add getCourses and getCoursesBySlug

--- src/stores/CourseStore.js
+++ src/stores/CourseStore.js
@@ -1,6 +1,10 @@
 import { EventEmitter } from "events";
+import Dispatcher from "../appDispatcher";
+import actionTypes from "../actions/actionTypes";
 
 const CHANGE_EVENT = "change";
+let _courses = [];
+
 class CourseStore extends EventEmitter {
   addChangeListener(callback) {
     this.on(CHANGE_EVENT, callback);
@@ -13,8 +17,28 @@ class CourseStore extends EventEmitter {
   emitChange() {
     this.emit(CHANGE_EVENT);
   }
+
+  getCourses() {
+    return _courses;
+  }
+
+  getCourseBySlug(slug) {
+    return _courses.find(course => course.slug === slug);
+  }
 }
 
 const store = new CourseStore();
+
+Dispatcher.register(action => {
+  switch (action.actionType) {
+    case actionTypes.CREATE_COURSE:
+      _courses.push(action.course);
+      store.emitChange();
+      break;
+
+    default:
+      break;
+  }
+});
 
 export default store;
